b748cafcf7b22ac059a60366a6a5aaa078111f6c
Lukas Oppermann <lukas@vea.re>
2014-06-10 08:01:01 +0200

    adding parent and domParent to childrens prototype

--- functions/children.js
+++ functions/children.js
@@ -16,46 +16,52 @@
 		})(window.Element.prototype);
 	}
 	// Module
-	var children = function(selector, maxLvl){
-		engine.selection = [];
-	  this.forEach(function(el){
-			var children = el.children, level = 0,
-			findChild = function( children ){
-				if( children !== undefined && (maxLvl === undefined || maxLvl === false || maxLvl > level ) )
-				{
-          level++;
-          for(var i = 0; i < children.length; i++ ){
-						if(selector === undefined || selector === false || children[i].matches(selector)){
-							children[i].prototype = {
-                depth : level
-              };
-							// check if child is in array
-							if(engine.selection.indexOf(children[i]) === -1){
-              	engine.selection.push(children[i]);
-							}
-            }
-            findChild(children[i].children);
-          }
-          level--;
-				}else{
-          level--;
-          return;
-        }
-      };
-      findChild(children);
-		});
-		// return a chainable engine object
-		return engine.fn.chain();
-	};
+	// var children = 
 	// export module
 	if ( typeof define === "function" && define.amd ) {		
 		define(["engine/engine"], function(engine){
-			engine.fn.children = children;
+			engine.fn.children = function(selector, maxLvl){
+				engine.selection = [];
+			  this.forEach(function(el){
+					var children = el.children, level = 0, p = undefined,
+					findChild = function( children, parent ){
+						if( children !== undefined && (maxLvl === undefined || maxLvl === false || maxLvl > level ) )
+						{
+		          level++;
+		          for(var i = 0; i < children.length; i++ ){
+								if(selector === undefined || selector === false || children[i].matches(selector)){
+		              children[i].prototype = {
+		              	depth: level
+		              };
+									if( parent )
+									{
+										children[i].prototype.parent = parent.parent;
+										children[i].prototype.domParent = parent.domParent;
+									}
+									p = children[i];
+									// check if child is in array
+									if(engine.selection.indexOf(children[i]) === -1){
+		              	engine.selection.push(children[i]);
+									}
+		            }
+		            findChild(children[i].children, {'parent':p,'domParent': children[i]});
+		          }
+		          level--;
+						}else{
+		          level--;
+		          return;
+		        }
+		      };
+		      findChild(children, {'parent': undefined, 'domParent': el});
+				});
+				// return a chainable engine object
+				return engine.fn.chain();
+			};
 			return engine;
 		});
 	}
-	else{ 
-		engine.fn.children = children;		
-	}
+	// else{ 
+	// 	engine.fn.children = children;		
+	// }
 
 })(window);
