b3900e818c0c32d689be485135be13536a32f355
Lukas Oppermann <lukas@vea.re>
2014-06-23 10:25:05 +0200

    add new method replaceclass

---
+++ functions/replaceclass.js
@@ -0,0 +1,29 @@
+(function(define, undefined){
+	// POLYFILLS
+	if (!String.prototype.trim) {
+	  String.prototype.trim = function () {
+	    return this.replace(/^\s+|\s+$/g, '');
+	  };
+	}
+	// fallback for define
+	if ( typeof define !== "function" || !define.amd ) {
+		define = function(arr, fn){
+			fn.call(window, window.engine);
+		};
+	}
+	// export module
+	define(["engine/engine"], function(engine){
+		// Module: replaceClass
+		engine.fn.replaceClass = function(regex, replace){
+			if( regex !== undefined && regex.trim().length > 0 ){
+				var regx = new RegExp('(' + regex + ')', 'g');
+				this.forEach(function(el, i){
+					el.className = el.className.replace(regx, replace).trim();
+				});
+			}
+			return this;
+		};
+		return engine;
+	});
+	//	
+}(window.define));
