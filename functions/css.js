e70c8f405173ad1be1e3e09b303b2868f4cec9d3
Lukas Oppermann <lukas@vea.re>
2014-07-13 13:04:54 +0200

    fix for no attribute

--- functions/css.js
+++ functions/css.js
@@ -9,22 +9,25 @@
 	define(["engine/engine"], function(engine){
 		// Module: css getter and setter
 		engine.fn.css = function(attr, val){
-			if( val === undefined )
+			if( attr !== undefined )
 			{
-		    if ('getComputedStyle' in window)
-		    {
-					return window.getComputedStyle(this[0], null).getPropertyValue(attr).replace(/^px+|px+$/g, '');
-		    }
-		    else if ('currentStyle' in window.element)
-		    {
-					return this[0].currentStyle[attr].replace(/^px+|px+$/g, '');
-		    }
-			}
-			else
-			{
-				this.forEach(function(el, i){
-					el.style[attr] = val;
-				});
+				if( val === undefined )
+				{
+			    if ('getComputedStyle' in window)
+			    {
+						return window.getComputedStyle(this[0], null).getPropertyValue(attr).replace(/^px+|px+$/g, '');
+			    }
+			    else if ('currentStyle' in window.element)
+			    {
+						return this[0].currentStyle[attr].replace(/^px+|px+$/g, '');
+			    }
+				}
+				else
+				{
+					this.forEach(function(el, i){
+						el.style[attr] = val;
+					});
+				}
 			}
 	    // return properties
 			return this;
