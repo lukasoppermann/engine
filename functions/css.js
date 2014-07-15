4f1adca94e235047711cf5c89c271e8a8447f9e9
Lukas Oppermann <lukas@vea.re>
2014-07-15 09:48:46 +0200

    fix to convert numbers to Int or float

--- functions/css.js
+++ functions/css.js
@@ -11,16 +11,26 @@
 		engine.fn.css = function(attr, val){
 			if( attr !== undefined )
 			{
-				if( val === undefined && typeof(attr) !== 'object' )
+				if( (val === undefined || val === 'float') && typeof(attr) !== 'object' )
 				{
-			    if ('getComputedStyle' in window)
-			    {
-						return window.getComputedStyle(this[0], null).getPropertyValue(attr).replace(/^px+|px+$/g, '');
-			    }
-			    else if ('currentStyle' in window.element)
-			    {
-						return this[0].currentStyle[attr].replace(/^px+|px+$/g, '');
-			    }
+					var float = (val === 'float' ? true : false);
+					if ('getComputedStyle' in window)
+					{
+						val = window.getComputedStyle(this[0], null).getPropertyValue(attr).replace(/^px+|px+$/g, '');
+					}
+					else if ('currentStyle' in window.element)
+					{
+						val = this[0].currentStyle[attr].replace(/^px+|px+$/g, '');
+					}
+					// if not a number
+					if( !isNaN(val) ){
+						if( float === true ){
+							return parseFloat(val);
+						}
+						return parseInt(val);
+					}
+					
+					return val;
 				}
 				else
 				{
